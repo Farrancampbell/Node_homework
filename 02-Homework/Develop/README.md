f859f4db240455442809bac851e03660fad65991
Farrancampbell <farranerinandcrew@gmail.com>
2021-04-10 02:13:05 -0400

    Final Commit

--- 02-Homework/Develop/README.md
+++ 02-Homework/Develop/README.md
@@ -1,10 +1,24 @@
-# **Hello**
-  ## Email: *ahldfk*
-  ## GitHub Username: *Farrancampbell* 
+# **README GENERATOR**
+  
+  **Description**
+  ## This is a project in which it creates a readme markdown in the Terminal using Node.js
+
   ## Apache
-  ## dflhafk
-  ## -Command To Install Dependancies :  hfwkfh
-  ## -To Run Tests You have to type in : dfjkhrkgh 
-  ## -To use this Repo, please note that, fkkfah 
-  ## If you want to contribute to this repo be mindful of, kfakh'
+  
+  **Installation**
+  ## -Command To Install Dependancies :  npm i
+
+  #**Usage**
+  ## -To use this Repo, please note that, Please Run npm i 
+
+  #**Contributing**
+  ## If you want to contribute to this repo be mindful of, Please leave nice comments :)
+
+  #**Tests**
+  ## -To Run Tests You have to type in : npm test 
+  
+  #**Questions**
+  ## Email: *farrancampbell@gmail.com*
+  ## GitHub Username: *farrancampbell* 
+  [GitHub](www.github.com/farrancampbell "GitHub Page")'
 
--- 02-Homework/Develop/utils/generateMarkdown.js
+++ 02-Homework/Develop/utils/generateMarkdown.js
@@ -2,8 +2,9 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license !== 'None') {
-    return ''; 
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
   }
+  return '';
 }
 
 // TODO: Create a function that returns the license link
@@ -26,14 +27,28 @@ function renderLicenseBadge(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# **${data.title}**
+  
+  **Description**
+  ## ${data.description}
+
+  ## ${data.license}
+  
+  **Installation**
+  ## -Command To Install Dependancies :  ${data.dependancies}
+
+  #**Usage**
+  ## -To use this Repo, please note that, ${data.user} 
+
+  #**Contributing**
+  ## If you want to contribute to this repo be mindful of, ${data.contributing}
+
+  #**Tests**
+  ## -To Run Tests You have to type in : ${data.command} 
+  
+  #**Questions**
   ## Email: *${data.email}*
   ## GitHub Username: *${data.name}* 
-  ## ${data.license}
-  ## ${data.description}
-  ## -Command To Install Dependancies :  ${data.dependancies}
-  ## -To Run Tests You have to type in : ${data.command} 
-  ## -To use this Repo, please note that, ${data.user} 
-  ## If you want to contribute to this repo be mindful of, ${data.contributing}'
+  [GitHub](${data.link} "GitHub Page")'
 
 `;
 }
