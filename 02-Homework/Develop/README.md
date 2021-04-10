020dfcd24769542ee909651836d44e2939e452e7
Farrancampbell <farranerinandcrew@gmail.com>
2021-04-10 00:40:54 -0400

    fixing license issues

--- 02-Homework/Develop/README.md
+++ 02-Homework/Develop/README.md
@@ -1,10 +1,10 @@
-# **hero**
-  ## Email: *dkfa*
-  ## GitHub Username: *fh* 
+# **Hello**
+  ## Email: *ahldfk*
+  ## GitHub Username: *Farrancampbell* 
+  ## Apache
+  ## dflhafk
+  ## -Command To Install Dependancies :  hfwkfh
+  ## -To Run Tests You have to type in : dfjkhrkgh 
+  ## -To use this Repo, please note that, fkkfah 
+  ## If you want to contribute to this repo be mindful of, kfakh'
 
-  ## fhs
-
-  ## -Command To Install Dependancies :  fkahg
-  ## -To Run Tests You have to type in : kfshd 
-  ## -To use this Repo, please note that, kjsfh 
-  ## If you want to contribute to this repo be mindful of, kfajh
--- 02-Homework/Develop/utils/generateMarkdown.js
+++ 02-Homework/Develop/utils/generateMarkdown.js
@@ -1,38 +1,41 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if(license == "None") 
-  {return " "}
+  if (license !== 'None') {
+    return ''; 
+  }
 }
+
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if(license == "MIT"){
-    return [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+function renderLicenseBadge(license) {
+  if (license !== 'MIT') {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
   }
-
+  return '';
 }
-
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'Apache') {
+    return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]`;
+  }
+  return '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# **${data.title}**
   ## Email: *${data.email}*
   ## GitHub Username: *${data.name}* 
-
+  ## ${data.license}
   ## ${data.description}
-
   ## -Command To Install Dependancies :  ${data.dependancies}
   ## -To Run Tests You have to type in : ${data.command} 
   ## -To use this Repo, please note that, ${data.user} 
-  ## If you want to contribute to this repo be mindful of, ${data.contributing}
-  [GitHub Link]($(data.))'
-},
-;
+  ## If you want to contribute to this repo be mindful of, ${data.contributing}'
 
+`;
 }
 
 module.exports = generateMarkdown;
